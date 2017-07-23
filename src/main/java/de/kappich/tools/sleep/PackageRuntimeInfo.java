fad0ed3437f6f986ee918a2054b974ede6698c80
Uwe Peuker <dev@upeuker.net>
2017-07-23 17:12:28 +0200

    Übernahme Version 3.9.2

--- src/main/java/de/kappich/tools/sleep/PackageRuntimeInfo.java
+++ src/main/java/de/kappich/tools/sleep/PackageRuntimeInfo.java
@@ -2,10 +2,10 @@
             package de.kappich.tools.sleep;
             public class PackageRuntimeInfo {
             public static String getLicence() { return "GPL";}
-            public static String getRelease() { return "Kernsoftware 3.9.1";}
-            public static String getVersion() { return "3.9.1";}
-            public static String getRevision() { return "34e82dc532c8d9bd6c65dd9b7ced5ccd7b031482";}
-            public static String getCompileTime() { return "03.08.2016 20:25:13";}
+            public static String getRelease() { return "Kernsoftware 3.9.2";}
+            public static String getVersion() { return "3.9.2";}
+            public static String getRevision() { return "866ec7f8528df5f1c8965c1d289385ec3737472a";}
+            public static String getCompileTime() { return "26.05.2017 11:27:37";}
             public static String getDependsOnCompiled() { return "de.bsvrz.sys.funclib.debug, de.bsvrz.sys.funclib.commandLineArgs";}
             public static String getDependsOnSource() { return "";}
             public static String getDependsOnLib() { return "";}
